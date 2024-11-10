8c4c1a829e887b33e1864a9c2874ef68de15b55c
Yeongrok Song <song7868@gmail.com>
2024-11-10 20:59:34 +0900

    feat: urls.py && templates echo_page.html 생성 간단한 tests코드 수행

--- djangoChat/app/tests.py
+++ djangoChat/app/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class EchoPageTests(TestCase):
+    def test_echo_page_status_code(self):
+        response = self.client.get(reverse('echo_page'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_echo_page_template(self):
+        response = self.client.get(reverse('echo_page'))
+        self.assertTemplateUsed(response, 'app/echo_page.html')
