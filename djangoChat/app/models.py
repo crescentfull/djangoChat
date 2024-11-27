f701517345ba29017c1870ad6dfb6e574f1c5fcc
Yeongrok Song <song7868@gmail.com>
2024-11-28 05:29:52 +0900

    feat: model

--- djangoChat/app/models.py
+++ djangoChat/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    
+    class Meta:
+        ordering = ["-id"] #역순 정렬 위해서
