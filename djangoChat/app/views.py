16a7a2417b2070abdd5219336468fd055aa0f8e6
Yeongrok Song <song7868@gmail.com>
2024-11-29 21:05:12 +0900

    feat: post api

--- djangoChat/app/views.py
+++ djangoChat/app/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from djangoChat.app.models import Post
 
 def echo_page(request):
     return render(request, "app/echo_page.html")
+
+def liveblog_index(request: HttpRequest) -> HttpResponse:
+    post_list = Post.objects.all()
+    return render(request, "app/liveblog_index.html", {
+        "post_list": post_list,
+    })
+    
+def post_partial(request: HttpRequest, post_id) -> HttpResponse:
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, "app/partial/post.html", {
+        "post":post,
+    })
