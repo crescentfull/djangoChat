b615879c56e9707e2cc83270785d393e631898f5
Yeongrok Song <song7868@gmail.com>
2024-11-30 23:02:05 +0900

    fix: models

--- djangoChat/app/models.py
+++ djangoChat/app/models.py
@@ -1,11 +1,45 @@
 from django.db import models
+from django.urls import reverse
+from app.mixins import ChannelLayerGroupSendMixin
 
-# Create your models here.
-from django.db import models
-
-class Post(models.Model):
+class Post(ChannelLayerGroupSendMixin, models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     
     class Meta:
         ordering = ["-id"] #역순 정렬 위해서
+        
+def post__on_post_save(instance: Post, created: bool, **kwargs):
+    if created:
+        message_type = "liveblog.post.created"
+    else:
+        message_type = "liveblog.post.updated"
+    
+    post_id = instance.pk
+    post_partial_url = reverse("post_partial", args=[post_id])
+    
+    instance.channel_layer_group_send({
+        "type": message_type,
+        "post_id": post_id,
+        "post_partial_url": post_partial_url,
+    })
+
+post_save.connect(
+    post__on_post_save,
+    sender=Post,
+    dispatch_uid="post__on_post_save"
+)
+    
+def post__on_post_delete(instance: Post, **kwargs):
+    post_id = instance.pk
+    
+    instance.channel_layer_group_send({
+        "type": "liveblog.post.deleted",
+        "post_id": post_id
+    })
+    
+post_delete.connect(
+    post__on_post_delete,
+    sender=Post,
+    dispatch_uid="post__on_post_delete"
+)
