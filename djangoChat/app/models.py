e15d35595f502c640e029b0cb31fe08946cef253
Yeongrok Song <song7868@gmail.com>
2024-12-01 18:08:31 +0900

    fix: models

--- djangoChat/app/models.py
+++ djangoChat/app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from django.db.models.signals import post_save, post_delete
+
 from app.mixins import ChannelLayerGroupSendMixin
 
 class Post(ChannelLayerGroupSendMixin, models.Model):
