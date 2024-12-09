6efbe189cc126df32b9997c547baba4ab7326ea4
Yeongrok Song <song7868@gmail.com>
2024-12-10 01:17:43 +0900

    feat: chat 관련 html

--- djangoChat/app/urls.py
+++ djangoChat/app/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path("", views.echo_page, name='echo_page'),
     path("liveblog/",views.liveblog_index),
     path("liveblog/posts/<int:post_id>/", views.post_partial, name="post_partial"),
-    path('chat/', include('chat.urls')),
 ]
