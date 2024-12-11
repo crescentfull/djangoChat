19a4ca251becc76fce5a151fa056a268eed3e8f5
Yeongrok Song <song7868@gmail.com>
2024-12-11 13:59:44 +0900

    feat: routing 채팅방 url

--- djangoChat/app/routing.py
+++ djangoChat/app/routing.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from app.consumers import EchoConsumer, LiveblogConsumer
+from chat import consumers
 
 websocket_urlpatterns = [
     path("ws/echo/", EchoConsumer.as_asgi()),
     path("ws/liveblog/", LiveblogConsumer.as_asgi()),
+    path("ws/chat/<str:room_name>/chat/", consumers.ChatConsumer.as_asgi()),
 ]
