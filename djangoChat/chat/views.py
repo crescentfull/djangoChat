0ff26ebbeca373397c6c77aec440492809881106
Yeongrok Song <song7868@gmail.com>
2024-12-19 19:45:35 +0900

    fix: 채팅방 에러 시작 인덱스화면이 index.html으로 렌더 템플릿을 했어야했는데 채팅방 템플릿으로 설정해놓았더라; 그것을 바꿔주니 Index.html에서 채팅방 이름이 넘어갈수 있었기 때문에 정상 작동함; 바보

--- djangoChat/chat/views.py
+++ djangoChat/chat/views.py
@@ -2,7 +2,7 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "chat/room_chat.html")
+    return render(request, "chat/index.html")
 
 def room_chat(request: HttpRequest, room_name: str) -> HttpResponse:
     return render(request, "chat/room_chat.html", {
