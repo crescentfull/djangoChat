bf2bb68663ae438a6e070d653888a822d40f4608
Yeongrok Song <song7868@gmail.com>
2025-01-06 19:29:45 +0900

    feat: 로그인 뷰 구현

--- djangoChat/accounts/views.py
+++ djangoChat/accounts/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import TemplateView, CreateView
+from django.urls import reverse_lazy
 
 # 프로필 뷰 호출은 로그인 시에만 의미가 있다.
+signup = CreateView.as_view(
+    form_class=UserCreationForm,
+    template_name="partials/form.html",
+    extra_context={
+        "form_name": "회원가입",
+        "submit_label": "회원가입",
+    },
+    success_url=reverse_lazy("accounts:login"),
+)
 
 login = LoginView.as_view(
     template_name = "partials/form.html",
