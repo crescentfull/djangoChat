8af55c315ccac3f3204f21c2d2754e94cebe4122
Yeongrok Song <song7868@gmail.com>
2025-01-06 19:01:07 +0900

    fix: 로그인 프로필 뷰 구현

--- djangoChat/accounts/views.py
+++ djangoChat/accounts/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+
+# 프로필 뷰 호출은 로그인 시에만 의미가 있다.
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = "accounts/profile.html"
 
 login = LoginView.as_view(
     template_name = "partials/form.html",
