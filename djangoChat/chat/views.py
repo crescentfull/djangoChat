18f784febf09d5d53c87c70311b8cf19d5fe42f5
Yeongrok Song <song7868@gmail.com>
2025-01-06 19:34:02 +0900

    fix: 로그인 데코레이터 구현

--- djangoChat/chat/views.py
+++ djangoChat/chat/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 from chat.forms import RoomForm
 from chat.models import Room
@@ -13,6 +14,7 @@ def index(request: HttpRequest) -> HttpResponse:
         "room_list" : room_qs,
     })
 
+@login_required
 def room_new(request):
     if request.method == "POST":
         form = RoomForm(request.POST)
@@ -26,7 +28,8 @@ def room_new(request):
     return render(request, "chat/room_form.html", {
         "form": form,
     }) 
-        
+
+@login_required        
 def room_chat(request: HttpRequest, room_pk: str) -> HttpResponse:
     room = get_object_or_404(Room, pk=room_pk)
     return render(request, "chat/room_chat.html", {
