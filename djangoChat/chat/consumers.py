32340eecc281afdd27251ae41280d692ac2de520
Yeongrok Song <song7868@gmail.com>
2025-01-13 21:36:40 +0900

    feat: consumers websocket 을 통해서 chat.user.join/ chat.user.leave 이벤트 전달

--- djangoChat/chat/consumers.py
+++ djangoChat/chat/consumers.py
@@ -1,7 +1,7 @@
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import JsonWebsocketConsumer
 from chat.models import Room
-
+from django.contrib.auth.models import AnonymousUser
 class ChatConsumer(JsonWebsocketConsumer):
     # room_name에 상관없이 모든 유저들을 광장(sqaure)을 통해 채팅
     #SQUARE_GROUP_NAME = "square"
@@ -12,6 +12,7 @@ class ChatConsumer(JsonWebsocketConsumer):
         super().__init__()
         # 파이썬 클래스에서 인스턴스 변수는 생성자 내에서 정의
         self.group_name = "" # 인스턴스 변수 group_name 추가
+        self.room = None
         
     def connect(self):
         # asgi.py에서 AuthMiddlewareStack을 적용하지 않았다면 KeyError 발생
@@ -27,20 +28,43 @@ class ChatConsumer(JsonWebsocketConsumer):
             self.close()
         else:
             room_pk = self.scope["url_route"]["kwargs"]["room_pk"]
-            self.group_name = Room.make_chat_group_name(room_pk=room_pk)   
-            
-            async_to_sync(self.channel_layer.group_add)(
-                self.group_name, self.channel_name
-            )
-            self.accept()
-            
+            try:
+                self.room = Room.objects.get(pk=room_pk)
+            except Room.DoesNotExist:
+                self.close()
+            else:
+                self.group_name = self.room.chat_group_name
+                # 새로운 유저가 Room에 들어오면, 다른 유저들에게 알림
+                is_new_join = self.room.user_join(self.channel_name, user)
+                if is_new_join:
+                    async_to_sync(self.channel_layer.group_send)(
+                        self.group_name,
+                        {
+                            "type":"chat.user.join",
+                            "username":user.username,
+                        }
+                    )
+                self.accept()
+                    
     # 웹 소켓 클라이언트와 접속이 끊어졌을 때, 호출됨
     def disconnect(self, code):
         if self.group_name:
             async_to_sync(self.channel_layer.group_discard)(
                 self.group_name, self.channel_name
             )
-    
+
+        user = self.scope["user"]
+        
+        if self.room is not None:
+            is_last_leave = self.room.user_leave(self.channel_name, user)
+            if is_last_leave:
+                async_to_sync(self.channel_layer.group_send)(
+                    {
+                    "type":"chat.user.leave",
+                    "username":user.username,
+                    }
+                )
+        
     #단일 클라이언트로부터 메세지를 받으면 호출
     def receive_json(self, content, **kwargs):
         user = self.scope["user"]
@@ -73,10 +97,21 @@ class ChatConsumer(JsonWebsocketConsumer):
             "sender": message_dict["sender"]
         })
     
-    
     # 채팅방이 삭제되면 호출됨
     # 웹소켓 클라이언트로 메세지를 전달하여 클라이언트에 의해 웹소켓 연결을 끊도록 함
     def chat_room_deleted(self, message_dict):
         self.send_json({
             "type": "chat.room.deleted",
+        })
+        
+    def chat_user_join(self, message_dict):
+        self.send_json({
+            "type":"chat.user.join",
+            "username":message_dict["username"],
+        })
+    
+    def chat_user_leave(self, message_dict):
+        self.send_json({
+            "type":"chat.user.leave",
+            "username":message_dict["username"],
         })
