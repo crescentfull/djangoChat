936a740d2f7b216fb0cd4f23f30a663c2e3c5855
Yeongrok Song <song7868@gmail.com>
2025-01-13 18:57:20 +0900

    feat: Room 모델 확장

--- djangoChat/chat/models.py
+++ djangoChat/chat/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_delete
+from django.db.models import QuerySet
+
 from djangoChat.mysite import settings
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
@@ -61,3 +63,24 @@ class RoomMember(models.Model):
                                      encoder=ExtendedEncoder,
                                      decoder=ExtendedDecoder,
                                      )
+    
+class OnlineUserMixin(models.Model):
+    class Meta:
+        abstract = True
+    
+    online_user_set = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        # 하나의 관계를 저장할 모델 지정
+        through="RoomMember",
+        # 모든 모델필드의 blank 디폴트값은 False
+        blank=True,
+        # user.joined_room_set.all() 코드로 user가 참여한 모든 Room 목록을 조회
+        related_name="joined_room_set",
+        )
+    
+    def get_online_user_set(self) -> QuerySet:
+        return self.online_user_set.all()
+    
+    def get_online_usernames(self) -> list[str]:
+        qs = self.get_online_users().values_list("username", flat=True)
+        return list(qs)
