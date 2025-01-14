b86da00a26dc1323dc26280408e954c25d49c93d
Yeongrok Song <song7868@gmail.com>
2025-01-14 17:16:47 +0900

    feat: users url path 구현

--- djangoChat/chat/urls.py
+++ djangoChat/chat/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("<str:room_pk>/chat/", views.room_chat, name="room_chat"), # room_name => room_pk
     path("new/", views.room_new, name="room_new"),
     path("<int:room_pk>/delete/", views.room_delete, name="room_delete"),
+    path("<int:room_pk>/users", views.room_users, name="room_users"),
 ]
