b176e981bc95f59d3ef6d1faa358fa85076a2756
Yeongrok Song <song7868@gmail.com>
2025-02-02 18:39:55 +0900

    fix: post 함수 httpRequest 삭제

--- djangoChat/app/views.py
+++ djangoChat/app/views.py
@@ -11,9 +11,9 @@ def liveblog_index(request: HttpRequest) -> HttpResponse:
     return render(request, "app/liveblog_index.html", {
         "post_list": post_list,
     })
-    
-def post_partial(request: HttpRequest, post_id) -> HttpResponse:
+
+def post_partial(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     return render(request, "app/partial/post.html", {
-        "post":post,
+        "post": post,
     })
